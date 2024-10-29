57f5ba5b4fe7478dc6922c030e05eb73a93d0dce
semhar-z <semharzewoldi@gmail.com>
2024-10-29 10:28:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Importing the data from our fake database files.
+// const users = require("./data/users");
+// const posts = require("./data/posts");
+
+app.get("/", (req, res) => {
+    res.send("Work in progress!");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server listening on port: ${port}.`);
+  });
+  
